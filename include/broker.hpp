86ffc466b4d1fd62cea1132d6fe576c10c6f5502
Sergey Vinogradov <vins2008@gmail.com>
2013-11-26 15:11:38 +0400

    hpp fixed

--- include/broker.hpp
+++ include/broker.hpp
@@ -1,12 +1,15 @@
 #ifndef BROKER_HPP
 #define BROKER_HPP
 
+
 #include <set>
 #include <map>
 #include <string>
 #include <vector>
 #include <deque>
+#include <list>
 
+#include "forwards.hpp"
 #include "context.hpp"
 #include "socket.hpp"
 #include <ctime>
@@ -16,12 +19,13 @@ namespace zbroker {
 
 
 class broker_t {
-    typedef std::set<std::string> addresses_t;
-    typedef std::map<std::string, addresses_t> consumers_t;
+
 
     typedef boost::shared_ptr<zmq::message_t> msg_ptr_t;
-    typedef std::vector<msg_ptr_t> msg_pack_t;
-    typedef std::map<std::string, std::deque<msg_pack_t> > pending_messages_t;
+
+    typedef std::set<msg_ptr_t> addresses_t;
+    typedef std::map<std::string, addresses_t> consumers_t;
+
 
     struct recipient_t {
         msg_ptr_t identity;
@@ -29,12 +33,21 @@ class broker_t {
     };
 
     struct service_t {
+        explicit service_t(const std::string& name)
+            : name (name)
+        {
+        }
+
+        void dispatch(const socket_ptr_t& backend, message_pack_t&& message);
+
         std::string name;
-        std::deque<msg_pack_t> messages;
+        std::deque<message_pack_t> messages;
         std::deque<recipient_t> waiting;
     };
 
     typedef boost::shared_ptr<service_t> service_ptr_t;
+    typedef std::map<std::string, service_ptr_t> service_map_t;
+
 
 public:
     broker_t(const context_ptr_t& ctx);
@@ -42,24 +55,30 @@ public:
     void run();
 
 private:
-    bool handle_frontend(socket_t& frontend);
-    void append_pending_messages(const std::string& destination, msg_pack_t&& messages);
+    bool handle_request();
+    bool handle_sender(zmq::message_t &sender);
+    bool handle_reciever(zmq::message_t &reciever);
+
+    service_ptr_t lookup_service(const std::string& name);
+
+   /* void append_pending_messages(const std::string& destination, msg_pack_t&& messages);
 
     bool handle_backend(socket_t& backend);
-    void append_consumer(const std::string& destination, std::string&& consumer_address);
+    void append_consumer(const std::string& destination, msg_ptr_t consumer_address);
 
     bool distribute_messages(socket_t& backend);
-    bool send_msg_pack(socket_t& backend, const std::string& address, const msg_pack_t& messages);
+    bool send_msg_pack(socket_t& backend, const msg_ptr_t& address, const msg_pack_t& messages);*/
 
 private:
     context_ptr_t m_ctx;
 
-    consumers_t m_consumers;
-    pending_messages_t m_pending_messages;
+    //consumers_t m_consumers;
+    //pending_messages_t m_pending_messages;
+
 
     // socket for senders and recievers
     socket_ptr_t m_socket;
-    std::map<std::string, service_ptr_t> m_services;
+    service_map_t m_services;
 
 
 
